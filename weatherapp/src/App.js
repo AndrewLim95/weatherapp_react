3bc443c1bda54ac49bf4102983fa0a8f58efc43b
Andrew <xemnasq@gmail.com>
2022-11-09 09:14:58 +0800

    added show button for individual country

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,9 +1,26 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const CountryData = ({country}) => {
+  return (
+    <div key={country.id}>
+    <h1>{country.name.common}</h1>
+    <p>{country.capital}</p>
+    <p>{country.area}</p>
+    <h2>Languages:</h2>
+    {Object.values(country.languages).map(language => 
+    <li key={language.id}>{language}</li>
+    )
+    }
+    <img src={country.flags.png} alt="country flag" />
+    </div>
+  )
+}
+
 const App = (props) => {
   const [countries, setCountries] = useState(props.countries) 
   const [newFilter, setNewFilter] = useState('')
+  const [show, setShow] = useState(false)
 
   useEffect(() => {
     axios
@@ -15,6 +32,10 @@ const App = (props) => {
 
   const handleFilterChange = (event) => {
     setNewFilter(event.target.value)
+  }
+
+  const showMessage = (bool) => {
+    setShow({show: bool})
   }
 
   const filteredCountry = countries.filter(country => country.name.common.toLowerCase().includes(newFilter.toLowerCase()))
@@ -39,19 +60,9 @@ const App = (props) => {
           value={newFilter}
           onChange={handleFilterChange}/>
           <h2>Country data</h2>
-        {filteredCountry.map(country => 
-          <div key={country.id}>
-          <h1>{country.name.common}</h1>
-          <p>{country.capital}</p>
-          <p>{country.area}</p>
-          <h2>Languages:</h2>
-          {Object.values(country.languages).map(language => 
-          <li key={language.id}>{language}</li>
-          )
-          }
-          <img src={country.flags.png} alt="country flag" />
-          </div>
-        )}
+          {filteredCountry.map(country => 
+          <CountryData key={country.id} country={country}/>
+          )}
       </div>
     )
   }
@@ -63,8 +74,12 @@ const App = (props) => {
           value={newFilter}
           onChange={handleFilterChange}/>
         <h2>List of Countries:</h2>
+        {console.log(show)}
           {filteredCountry.map(country => 
-          <li key={country.id}>{country.name.common}</li>
+          <li key={country.id}>{country.name.common} 
+          <button onClick={showMessage.bind(null, true)}>show</button>
+          <button onClick={showMessage.bind(null, false)}>hide</button>
+          {show && <CountryData key={country.id} country={country}/>}</li>
           )}
       </div>
     )
